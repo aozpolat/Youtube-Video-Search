1ae779bf95c4f62a5682decd645f9fbd096a1062
aozpolat <akdrozpolat@gmail.com>
2021-09-04 19:36:01 +0300

    build basic structure

---
+++ videoSearcher.py
@@ -0,0 +1,17 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import _errors
+
+try:
+    videoUrl = input("Please enter the video url: \n")
+    videoId = videoUrl[videoUrl.index("v=") + 2 : ]
+    srt = YouTubeTranscriptApi.get_transcript(f"{videoId}")
+    # print(srt)
+    word = input("Please enter the word you are looking for: \n")
+    print("Link(s): ")
+    for index in range(len(srt)):
+        if word in srt[index]["text"]:
+            print(f"https://www.youtube.com/watch?v=zxYjTTXc-J8&t={srt[index]['start']}s")
+    
+except _errors.TranscriptsDisabled :  
+    print("This video has no subtitles")
+
